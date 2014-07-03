1557abc5d1a8d0fe79ca4b6991409c5ca82ea057
Micah Lucas <micah@scal.io>
2014-07-02 19:34:52 -0700

    Added Youtube login/upload functionality.

--- SecureShareUILibrary/src/io/scal/secureshareui/controller/YoutubeSiteController.java
+++ SecureShareUILibrary/src/io/scal/secureshareui/controller/YoutubeSiteController.java
@@ -1,14 +1,51 @@
-
 package io.scal.secureshareui.controller;
 
+import io.scal.secureshareui.login.YoutubeLoginActivity;
 import io.scal.secureshareui.model.Account;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Handler;
+import android.util.Log;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.googleapis.media.MediaHttpUploader;
+import com.google.api.client.googleapis.media.MediaHttpUploaderProgressListener;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.YouTubeRequestInitializer;
+import com.google.api.services.youtube.YouTubeScopes;
+import com.google.api.services.youtube.model.Video;
+import com.google.api.services.youtube.model.VideoSnippet;
+import com.google.api.services.youtube.model.VideoStatus;
 
 public class YoutubeSiteController extends SiteController {
     private static final String TAG = "YoutubeSiteController";
     public static final String SITE_NAME = "YouTube";
     public static final String SITE_KEY = "youtube";
+    private static final String CLIENT_ID = "279338940292-7pqin08vmde3nhheekijn6cfetknotbs.apps.googleusercontent.com";
+    
+    final HttpTransport transport = new NetHttpTransport();
+    final JsonFactory jsonFactory = new GsonFactory();
+    private YouTube mYoutube;
+    
+    private static String VIDEO_FILE_FORMAT = "video/*";
     
     public YoutubeSiteController(Context context, Handler handler, String jobId) {
         super(context, handler, jobId);
@@ -17,12 +54,112 @@ public class YoutubeSiteController extends SiteController {
     
     @Override
     public void startAuthentication(Account account) {
-        // TODO Auto-generated method stub
-        
+    	Intent intent = new Intent(mContext, YoutubeLoginActivity.class);
+        intent.putExtra(SiteController.EXTRAS_KEY_CREDENTIALS, account.getCredentials());
+        ((Activity) mContext).startActivityForResult(intent, SiteController.CONTROLLER_REQUEST_CODE); // FIXME not a safe cast, context might be a service 
     }
-    @Override
-    public void upload(String title, String body, String mediaPath, Account account, boolean useTor) {
-        // TODO Auto-generated method stub
-        
+    
+	@Override
+	public void upload(String title, String body, String mediaPath, Account account, boolean useTor) {	
+		List<String> scopes = new ArrayList<String>();
+		scopes.add(YouTubeScopes.YOUTUBE_UPLOAD);
+		
+		GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(super.mContext, scopes);
+		
+		//set google username
+		credential.setSelectedAccountName(account.getCredentials()); 
+		
+        mYoutube = new com.google.api.services.youtube.YouTube.Builder(transport, jsonFactory, credential)
+                        .setApplicationName("StoryMaker")
+                        .setGoogleClientRequestInitializer(new YouTubeRequestInitializer(CLIENT_ID))
+                        .build();
+ 
+        File mediaFile = new File(mediaPath);  
+        YouTube.Videos.Insert videoInsert = prepareUpload(title, body, mediaFile);
+        new VideoUploadAsyncTask().execute(videoInsert);
+	}
+
+    public YouTube.Videos.Insert prepareUpload(String title, String body, File videoFile) {
+        try {
+            // Add extra information to the video before uploading.
+            Video videoObjectDefiningMetadata = new Video();
+
+            // Set the video to public (default).
+            VideoStatus status = new VideoStatus();
+            status.setPrivacyStatus("private");
+            videoObjectDefiningMetadata.setStatus(status);
+
+            // We set a majority of the metadata with the VideoSnippet object.
+            VideoSnippet snippet = new VideoSnippet();
+
+            // Video file name.
+            snippet.setTitle(title);
+            snippet.setDescription(body);
+
+            // Set keywords.
+            List<String> tags = new ArrayList<String>();
+            tags.add("storymaker");
+            snippet.setTags(tags);
+
+            // Set completed snippet to the video object.
+            videoObjectDefiningMetadata.setSnippet(snippet);
+
+            InputStreamContent mediaContent = new InputStreamContent(VIDEO_FILE_FORMAT, new BufferedInputStream(new FileInputStream(videoFile)));
+            YouTube.Videos.Insert videoInsert = mYoutube.videos().insert("snippet,statistics,status", videoObjectDefiningMetadata, mediaContent);
+
+            // Set the upload type and add event listener.
+            MediaHttpUploader uploader = videoInsert.getMediaHttpUploader();
+            uploader.setDirectUploadEnabled(false);
+
+            MediaHttpUploaderProgressListener progressListener = new MediaHttpUploaderProgressListener() {
+                public void progressChanged(MediaHttpUploader uploader) throws IOException {
+                    switch (uploader.getUploadState()) {
+                        case INITIATION_STARTED:
+                            Log.d(TAG, "Upload file: Initiation Started");
+                            break;
+                        case INITIATION_COMPLETE:
+                            Log.d(TAG, "Upload file: Initiation Completed");
+                            break;
+                        case MEDIA_IN_PROGRESS:
+                            Log.d(TAG, "Upload file: Upload in progress");
+                            Log.d(TAG, "Upload file: Upload percentage: ");//uploader.getProgress());
+                            break;
+                        case MEDIA_COMPLETE:
+                            Log.d(TAG, "Upload file: Upload Completed!");
+                            break;
+                        case NOT_STARTED:
+                            Log.d(TAG, "Upload file: Upload Not Started!");
+                            break;
+                    }
+                }
+            };
+            
+            uploader.setProgressListener(progressListener);
+
+            return videoInsert;
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "File not found: " + e.getMessage());
+            return null;
+        } catch (IOException e) {
+            Log.e(TAG, "Progress IOException: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public class VideoUploadAsyncTask extends AsyncTask<YouTube.Videos.Insert, Void, Void> {
+        @Override
+        protected Void doInBackground( YouTube.Videos.Insert... inserts ) {
+            YouTube.Videos.Insert videoInsert = inserts[0];
+            try {
+                Video returnVideo = videoInsert.execute();
+            } catch (final GooglePlayServicesAvailabilityIOException availabilityException) {
+                //googleplay not available
+            } catch (UserRecoverableAuthIOException userRecoverableException) {
+                //startActivityForResult(userRecoverableException.getIntent(), TasksSample.REQUEST_AUTHORIZATION);
+            } catch (IOException e) {
+                Log.e(TAG, "AsyncTask IOException: " + e.getMessage());
+            }
+            return null;
+        }
     }
 }
--- SecureShareUILibrary/src/io/scal/secureshareui/login/YoutubeLoginActivity.java
+++ SecureShareUILibrary/src/io/scal/secureshareui/login/YoutubeLoginActivity.java
@@ -1,15 +1,163 @@
-
 package io.scal.secureshareui.login;
 
+import io.scal.secureshareui.controller.SiteController;
 import io.scal.secureshareuilibrary.R;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class YoutubeLoginActivity extends Activity {
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.youtube.YouTubeScopes;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube_login);
-    }
+@SuppressWarnings("deprecation")
+public class YoutubeLoginActivity extends Activity implements Runnable {
+
+	private static int mAccessResult = 0;
+	private static String mAccessToken = null;
+
+	static final String TAG = "YoutubeLoginActivity";
+	private static final String CLIENT_ID = "279338940292-7pqin08vmde3nhheekijn6cfetknotbs.apps.googleusercontent.com";//"422811616434-03efee9fu30quev60avpopvvg8osird3.apps.googleusercontent.com";
+	private static final String CLIENT_SECRET = "N3uZBIBjr__TPHUWUkmtBUYk";//UsH4wlEuM9eEy7K1VBbrvbH8";
+	
+	private static final String YOUTUBE_USER_INFO_URL = "https://www.googleapis.com/oauth2/v2/userinfo";
+	
+	HttpTransport transport = new NetHttpTransport();
+    private final JsonFactory jsonFactory = new GsonFactory();
+	
+	private String mReturnedWebCode;
+	private GoogleTokenResponse mAuthResp;
+	private static final String REDIRECT_URI = "http://localhost";
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_youtube_login);
+
+		login();
+	}
+
+	public void login() {
+		WebView webview = new WebView(this);
+		webview.getSettings().setJavaScriptEnabled(true);
+		webview.setVisibility(View.VISIBLE);
+		setContentView(webview);
+
+		List<String> scopes = new ArrayList<String>();
+		scopes.add(YouTubeScopes.YOUTUBE_UPLOAD);
+		scopes.add("https://www.googleapis.com/auth/userinfo.email");
+		
+		String authUrl = new GoogleAuthorizationCodeRequestUrl(CLIENT_ID, REDIRECT_URI, scopes).build();
+
+		// WebViewClient must be set BEFORE calling loadUrl!
+		webview.setWebViewClient(new WebViewClient() {
+
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap bitmap) {}
+
+			@Override
+			public void onPageFinished(WebView view, String url) {
+
+				if (url.startsWith(REDIRECT_URI)) {
+					if (url.indexOf("code=") != -1) {
+						if (mReturnedWebCode == null) {
+							mReturnedWebCode = extractCodeFromUrl(url);
+							view.setVisibility(View.INVISIBLE);
+							new Thread(YoutubeLoginActivity.this).start();
+						}
+					} else if (url.indexOf("error=") != -1) {
+						view.setVisibility(View.INVISIBLE);
+						setResult(RESULT_CANCELED);
+					}
+				}
+			}
+
+			private String extractCodeFromUrl(String url) {
+				return url.substring(REDIRECT_URI.length() + 7, url.length());
+			}
+		});
+
+		webview.loadUrl(authUrl);
+	}
+
+	public void run() {
+		try {
+			mAuthResp = new GoogleAuthorizationCodeTokenRequest(transport, 
+																jsonFactory, 
+																CLIENT_ID, 
+																CLIENT_SECRET, 
+																mReturnedWebCode, REDIRECT_URI).execute();
+			
+			String userEmail = getUserEmail(mAuthResp.getAccessToken());
+			
+	        if (null != userEmail) {
+	            mAccessResult = RESULT_OK;
+	            mAccessToken = userEmail;
+	        } else { // failed login
+	            mAccessResult = 0;
+	        }
+			
+		} catch (IOException ioe) {
+			Log.e(TAG, "Authentication Error: " + ioe.getMessage());
+		}
+
+		finish();
+	}
+	
+	public String getUserEmail(String accessToken) {
+		String userEmail = null;
+	    HttpClient httpClient = new DefaultHttpClient();
+	    HttpGet httpGet = new HttpGet(YOUTUBE_USER_INFO_URL + "?access_token=" + accessToken);
+
+	    try {
+	    	ResponseHandler<String> responseHandler = new BasicResponseHandler();
+	    	String responseBody = httpClient.execute(httpGet, responseHandler);
+			JSONObject responseJson = new JSONObject(responseBody);
+			
+			userEmail = responseJson.getString("email");
+	        
+	    } catch (ClientProtocolException cpe) {
+	    	Log.e(TAG, "ClientProtocolException: " + cpe.getMessage());
+	    } 
+	    catch (IOException ioe) {
+	    	Log.e(TAG, "IOException: " + ioe.getMessage());
+	    } 
+	    catch (JSONException jsone) {
+	    	Log.e(TAG, "JSONException: " + jsone.getMessage());
+	    }
+	    
+	    return userEmail;
+	}
+
+	public void finish() {
+		Intent data = new Intent();
+        data.putExtra(SiteController.EXTRAS_KEY_CREDENTIALS, mAccessToken);
+        setResult(mAccessResult, data);;
+
+		super.finish();
+	}
 }
